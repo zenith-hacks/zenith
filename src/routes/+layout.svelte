d693539c958d1463372699bb4b26af9659d83377
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2024-09-05 20:58:31 +0100

    optimise font loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,8 +35,8 @@
 
 <style>
   /* TODO: self-host these */
-  @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@700&display=swap&text=ABCDEFGHIJKLMNOPQRSTUVWXYZ%20ltienakcsgh205');
+  @import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:wght@400&display=swap&subset=latin');
 
   :global(:root) {
     font-family: 'Instrument Sans', system-ui, sans-serif;
