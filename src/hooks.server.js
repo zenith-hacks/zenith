a5f6f50f2192d3cf646054dce268c172f2359db7
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2024-09-07 01:09:55 +0100

    set link header so fonts can be prefetched while document is downloading

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,12 @@
+
+
 export async function handle({ event, resolve }) {
   const response = await resolve(event);
 
   // Disable JS preloading. This took hours to find.
   // I hate web development.
-  response.headers.delete('link');
+  // Also, use the link header to preload our fonts because they're the biggest things on the site.
+  response.headers.set('link', '</fonts/SpaceGrotesk-title-subset-instanced.woff2>; rel"preconnect", </fonts/InstrumentSans-asciionly-subset-instanced.woff2>; rel="preconnect"');
 
   return response;
 }
