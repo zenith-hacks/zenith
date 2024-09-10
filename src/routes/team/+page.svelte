95c0f8cc1d0fbdda0899ca1cb917220541e6eb8a
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2024-09-10 19:38:55 +0100

    initial commit for team branch

---
+++ src/routes/team/+page.svelte
@@ -0,0 +1,148 @@
+<script>
+  import Rushil from '$lib/people/Rushil.svg';
+  import Briyan from '$lib/people/Briyan.svg';
+  import Leo from '$lib/people/Leo.svg';
+  import Mahad from '$lib/people/Mahad.svg';
+  import Hamza from '$lib/people/Hamza.svg';
+  import Sarah from '$lib/people/Sarah.svg';
+  import Karl from '$lib/people/Karl.svg';
+</script>
+
+<main>
+  <h1>Meet the Zenith team</h1>
+
+  <article>
+    <img src={Rushil} alt="Rushil's avatar against a navy blue background. His head is a triangle" />
+    <div class="profile-card">
+      <h2>Rushil</h2>
+      <p class="tagline">Event lead</p>
+      <p class="description">Something something description here. They haven't answered me yet.</p>
+      <p>When asked for a colour and a shape, he picked navy blue and a triangle.</p>
+    </div>
+  </article>
+  
+  <article class="right">
+    <img src={Leo} alt="Leo's avatar against a dark purple background. His head is an octagon" />
+    <div class="profile-card">
+      <h2>Leo</h2>
+      <p class="tagline">Safeguarding lead</p>
+      <p class="description">Something something description here. They haven't answered me yet.</p>
+      <p>When asked for a colour and a shape, he picked purple and an octagon.</p>
+    </div>
+  </article>
+  
+  <article>
+    <img src={Sarah} alt="Sarah's avatar against a bright pink background. Her head is an icosahedron" />
+    <div class="profile-card">
+      <h2>Sarah</h2>
+      <p class="tagline">Data privacy coordinator, design lead and web developer</p>
+      <p class="description">Hi! I'm Sarah. I made the website you're looking at.</p>
+      <p>
+        She wasn't asked for a colour and a shape because she would be talking to herself.
+        She picked an icosahedron, and she thinks everyone else's choices are boring. (No offense.)
+      </p>
+    </div>
+  </article>
+  
+  <article class="right">
+    <img src={Hamza} alt="Hamza's avatar against a dark purple background. His head is an hexagon" />
+    <div class="profile-card">
+      <h2>Hamza</h2>
+      <p class="tagline">Web developer</p>
+      <p class="description">Something something description here. They haven't answered me yet.</p>
+      <p>When asked for a colour and a shape, he picked orange and a hexagon.</p>
+    </div>
+  </article>
+  
+  <article>
+    <img src={Mahad} alt="Mahad's avatar against a navy blue background. His head is a hexagon" />
+    <div class="profile-card">
+      <h2>Mahad</h2>
+      <p class="tagline">Sponsor outreach and developer</p>
+      <p class="description">I'm a 15-year-old high schooler and software engineer from the UK!</p>
+      <p>When asked for a colour and a shape, he picked light blue and a hexagon.</p>
+    </div>
+  </article>
+  
+  <article class="right">
+    <img src={Karl} alt="Karl's avatar against a green background. His head is missing" />
+    <div class="profile-card">
+      <h2>Karl</h2>
+      <p class="tagline">Hardware design</p>
+      <p class="description">Wdym</p>
+      <p>When asked for a colour and a shape, he was stubborn, which is why he doesn't have a head.</p>
+    </div>
+  </article>
+  
+  <article>
+    <img src={Briyan} alt="Briyan's avatar against a navy blue background. His head is a square" />
+    <div class="profile-card">
+      <h2>Briyan</h2>
+      <p class="tagline">Sponsor outreach</p>
+      <p class="description">Hey there! I'm Briyan Dyju from the United Arab Emirates and I'll be helping organising Zenith this season!</p>
+      <p>When asked for a colour and a shape, he was confused. We eventually settled on yellow and a square.</p>
+    </div>
+  </article>
+</main>
+
+<style>
+  main {
+    width: 1024px;
+    margin: auto;
+    padding: 8em 2em;
+    max-width: 100%;
+    
+    display: flex;
+    flex-direction: column;
+    gap: 4em;
+  }
+  
+  article {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    gap: 2em;
+  }
+  
+  @media (min-width: 1024px) {
+    article {
+      flex-direction: row;
+    }
+    
+    .right {
+      flex-direction: row-reverse;
+    }
+  }
+  
+  article > img {
+    flex: 0;
+    min-width: min-content;
+  }
+  
+  .profile-card {
+    flex: 1 1 0;
+    width: 100%;
+    padding: 1em;
+    background: rgb(var(--bg-20));
+    box-shadow: 0.5em 0.5em rgb(var(--bg-30));
+    min-width: min-content;
+  }
+  
+  p {
+    margin: 0.5em 0;
+  }
+  
+  .description {
+    padding-left: 1em;
+    border-left: 2px solid rgb(var(--fg));
+  }
+  
+  .description::before {
+    content: "\201C";
+  }
+  
+  .description::after {
+    content: "\201D";
+  }
+</style>
