a660418d766eba7bf3fe84ed772cba73a9ccaea5
Sarah C <144068104+iamawatermelo@users.noreply.github.com>
2024-09-10 15:22:42 +0100

    lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,9 @@ export default defineConfig({
   build: {
     // We don't use JavaScript like at all.
     // Move it out of the critical path.
-    modulePreload: false,
-
-    // We already inline our SVGs by making them Svelte components.
-    // Everything that is not supposed to be inlined is deliberate.
-    assetsInlineLimit: 0,
-
-    // Do as much as we can on the server.
-    ssr: true
+    modulePreload: false
+  },
+  css: {
+    transformer: "lightningcss"
   }
 });
